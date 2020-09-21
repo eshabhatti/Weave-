843afa051c07e15fcd182d3d429516e7d8b970be
Sahil Kapur <sahilkapur.a@gmail.com>
2020-09-20 17:53:47 -0700

    1.1.3 add form inputs as states that can be POSTed

--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 
 export default function Login(props) {
+  const [loginName, updateLoginName] = useState("");
+  const [password, updatePassword] = useState("");
+
+  const handleChange = (event) => {
+    const { target } = event;
+    const { value } = target;
+    props.update(value);
+    event.persist();
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const body = {
+      username: loginName,
+      password: password
+    }
+    console.log(body);
+    /*
+     * post to backend
+     */
+    // fetch();
+    alert(JSON.stringify(body));
+  }
+
   return (
     <div>
       <h1 className="login-page-title">Join the conversation</h1>
@@ -9,11 +33,20 @@ export default function Login(props) {
         <img src="./img/weave-icon.svg" className="login-icon" />
         <form className="login-form">
           <label className="login-form-label">username/email</label>
-          <input className="login-form-input"/>
+          <input
+            value={loginName}
+            onChange={e => updateLoginName(e.target.value)} className="login-form-input"
+          />
+
           <label className="login-form-label">password</label>
-          <input type="password" className="login-form-input"/>
+          <input
+            value={password}
+            onChange={e => updatePassword(e.target.value)}
+            type="password"
+            className="login-form-input" />
+          <button type="submit" className="login-submit-btn" onClick={(e) => onSubmit(e)}>login</button>
+
         </form>
-        <button className="login-submit-btn">login</button>
         <a href="#" className="login-link">create an account</a>
         <a href="#" className="login-link">forgot password</a>
       </div>
