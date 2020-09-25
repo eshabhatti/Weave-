710ae045cab4196bfd894111d53e54ee87a5b9d9
Mark Gindling <mgindlin@purdue.edu>
2020-09-25 11:57:07 -0400

    1.2.5 Mark -- Moved Login Function Added weavelogin.py for the login function. Also changed the error JSON returns there.

--- api/api.py
+++ api/api.py
@@ -14,6 +14,7 @@ from flask_cors import CORS
 from models import User
 from weavepost import weave_post
 from weaveregister import weave_register
+from weavelogin import weave_login
 
 # Initializes Flask
 app = Flask(__name__)
@@ -46,73 +47,13 @@ mysql.init_app(app)
 with app.app_context():
     app.register_blueprint(weave_post)
     app.register_blueprint(weave_register)
+    app.register_blueprint(weave_login)
 
 # # # # Backend code for TIME requests. 
 # # This is no longer implmented in the frontend, I believe.
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
-
-# # # # Backend code for LOGIN requests
-# Should expect a POST request with a JSON like this: {"username":"[username_or_email]","password":"[password]"}
-# Test basic functionality with the following script (on Windows):
-# curl -i -X POST -H "Content-Type:application/json" -d "{\"username\":\"testname\",\"password\":\"Gudpasswurd22\"}" http://localhost:5000/login/
-@app.route('/login/', methods=["GET", "POST"])
-def weave_login():
-
-    # The backend has received a login POST request.
-    if request.method == "POST":
-
-        # Checks for JSON format.
-        if (not request.is_json):
-            return "{\"error_message\":\"not_JSON\"}"
-        login_info = request.get_json()
-        
-        # Checks for the correct credentials.
-        if ("username" not in login_info or "password" not in login_info):
-            return "{\"error_message\":\"missing_element\"}"
-        
-        # Get username and password from JSON.
-        username = login_info["username"]
-        password = login_info["password"]
-
-        # Determines if username in the JSON relates to the username column or the email column
-        username_type = "username"
-        if (re.search("@", username) != None):
-           username_type = "email"
-           
-        # Need to pull the hashed password of the selected user out of the database
-        # This also needs to catch the case of an invalid user
-        cursor = mysql.connection.cursor()
-        cursor.execute("SELECT encrypted_password FROM UserAccount WHERE " + username_type + " = %s;", (username,)) #args have to be a tuple
-        if (cursor.rowcount == 0):
-            return "{\"error_message\":\"invalid_credentials\"}"
-        hashed_password = cursor.fetchall()
-        hashed_password = (hashed_password[0])["encrypted_password"]
-        
-        # This will validate the user's password:
-        valid_password = bcrypt.checkpw(password.encode('utf8'), hashed_password.encode('utf8')) #have to encode strings here
-        if (not valid_password):
-            return "{\"error_message\":\"invalid_credentials\"}"
-            
-        # This will fetch the user's information from the database after validation (should it all be grabbed in the first execute?)
-        cursor.execute("SELECT * FROM UserAccount WHERE " + username_type + " = %s", (username,))
-        account = cursor.fetchall()
-        print(account) # debugging
-
-        # Return "Profile page of account"
-        for row in cursor:
-            # creates an access token and refresh token using the usernamem
-            ret = {
-                'access_token': create_access_token(identity=row["username"]),
-                'refresh_token': create_refresh_token(identity=row["username"])
-            }
-            return jsonify(ret), 200
-        return "{\"error_message\": \"invalid_credentials\"}"
-
-    # Not a POST request
-    else:
-        return ("", 204)
 
 @app.route('/refresh', methods=['POST'])
 @jwt_refresh_token_required
---
+++ api/weavelogin.py
@@ -0,0 +1,67 @@
+# # # # Backend code for LOGIN requests
+# Should expect a POST request with a JSON like this: {"username":"[username_or_email]","password":"[password]"}
+# Test basic functionality with the following script (on Windows):
+# curl -i -X POST -H "Content-Type:application/json" -d "{\"username\":\"testname\",\"password\":\"Gudpasswurd22\"}" http://localhost:5000/login/
+from flask import Blueprint, request, redirect, url_for, jsonify
+from extensions import mysql
+import bcrypt
+import re
+
+weave_login = Blueprint('weave_login', __name__)
+
+@weave_login.route('/login/', methods=["GET", "POST"])
+def weave_user_login():
+
+    # The backend has received a login POST request.
+    if request.method == "POST":
+
+        # Checks for JSON format.
+        if (not request.is_json):
+            return jsonify({'error_message':'Request Error: Not JSON.'})  
+        login_info = request.get_json()
+        
+        # Checks for the correct credentials.
+        if ("username" not in login_info or "password" not in login_info):
+            return jsonify({'error_message':'Request Error: Missing JSON Element'})  
+        
+        # Get username and password from JSON.
+        username = login_info["username"]
+        password = login_info["password"]
+
+        # Determines if username in the JSON relates to the username column or the email column
+        username_type = "username"
+        if (re.search("@", username) != None):
+           username_type = "email"
+           
+        # Need to pull the hashed password of the selected user out of the database
+        # This also needs to catch the case of an invalid user
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT encrypted_password FROM UserAccount WHERE " + username_type + " = %s;", (username,)) #args have to be a tuple
+        if (cursor.rowcount == 0):
+            return jsonify({'error_message':'Username and password do not match.'}) 
+        hashed_password = cursor.fetchall()
+        hashed_password = (hashed_password[0])["encrypted_password"]
+        
+        # This will validate the user's password:
+        valid_password = bcrypt.checkpw(password.encode('utf8'), hashed_password.encode('utf8')) #have to encode strings here
+        if (not valid_password):
+            return jsonify({'error_message':'Username and password do not match.'}) 
+            
+        # This will fetch the user's information from the database after validation (should it all be grabbed in the first execute?)
+        cursor.execute("SELECT * FROM UserAccount WHERE " + username_type + " = %s", (username,))
+        account = cursor.fetchall()
+        print(account) # debugging
+
+        # Return "Profile page of account"
+        for row in cursor:
+            # creates an access token and refresh token using the usernamem
+            ret = {
+                'access_token': create_access_token(identity=row["username"]),
+                'refresh_token': create_refresh_token(identity=row["username"])
+            }
+            return jsonify(ret), 200
+        return jsonify({'error_message':'Username and password do not match.'})  
+
+    # Not a POST request
+    else:
+        return ("", 204)
