76eef9664460d80c6389f5e95af3bf3c452ba430
schikyal <42979122+schikyal@users.noreply.github.com>
2020-09-28 12:43:27 -0400

    1.4.1 Sai building better layout
    Sidebar yellow for testing

--- frontend/src/user_info/Profile/Profile.jsx
+++ frontend/src/user_info/Profile/Profile.jsx
@@ -19,15 +19,18 @@ export default function Profile() {
 
     return (
         <div>
+            {/* Bootstrap Stylesheet */}
             <link
                 rel="stylesheet"
                 href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
                 integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
                 crossorigin="anonymous"
             />
-            <Navbar expand="lg" bg="light" variant="light" sticky="top">
-                <Navbar.Brand>Weave</Navbar.Brand>
-                <img src="./img/weave-icon.svg" className="profile-icon" alt="" />
+            <Navbar expand="lg" bg="dark" variant="dark" sticky="top">
+                <Navbar.Brand href="login">
+                    <img src="./img/weave-icon.svg" width="50" height="50"
+                         className="d-inline-block align-top" alt="" />
+                </Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link>Messages</Nav.Link>
                     <Nav.Link>Notifications</Nav.Link>
@@ -35,29 +38,38 @@ export default function Profile() {
                 </Nav>
             </Navbar>
             <div className="profile-container">
-                <div classname="profile-info">
-                    {/* pull user data */}
-                    <h1 className="profile-name">Name</h1>
-                    <p className="profile-username">Username</p>
-                    {/* toggle active depending on who is viewing the page */}
-                    <button type="button" className="profile-follow-button">Follow</button>
-                    <p className="profile-followers"># Followers</p>
-                    <p className="profile-following"># Following</p>
-                    <h1 className="profile-bio-title">Bio</h1>
-                    <p className="profile-bio">Actual Bio</p>
-                    <h1 className="profile-about-title">About</h1>
-                    <p className="profile-about">About info</p>
-                </div>
-                <div className="profile-choice">
-                    <div className="profile-buttons">
-                        <button type="button" className="profile-posts-button">Posts</button>
-                        <button type="button" className="profile-interactions-button">Interactions</button>
+                <div className="profile-container">
+                    <div className="profile-menu">
+                        {/* need to fix */}
+                        <h1 className="profile-menu-title">Menu</h1>
                     </div>
-                    <div className="profile-display">
-                        {/* toggle between posts and int
-                        <PostScreen active={postsOrInt} />
-                        <InteractionScreen active={!postsOrInt} />
-                        */}
+                </div>
+                <div className="profile-container">
+                    <div className="profile-info">
+                        {/* pull user data */}
+                        <img src="./img/weave-icon.svg" classname="profile-icon" alt="" />
+                        <h1 className="profile-name">Name</h1>
+                        <p className="profile-username">Username</p>
+                        {/* toggle active depending on who is viewing the page */}
+                        <button type="button" className="profile-follow-button">Follow</button>
+                        <p className="profile-followers"># Followers</p>
+                        <p className="profile-following"># Following</p>
+                        <h1 className="profile-bio-title">Bio</h1>
+                        <p className="profile-bio">Actual Bio</p>
+                        <h1 className="profile-about-title">About</h1>
+                        <p className="profile-about">About info</p>
+                    </div>
+                    <div className="profile-choice">
+                        <div className="profile-buttons">
+                            <button type="button" className="profile-posts-button">Posts</button>
+                            <button type="button" className="profile-interactions-button">Interactions</button>
+                        </div>
+                        <div className="profile-display">
+                            {/* toggle between posts and int
+                            <PostScreen active={postsOrInt} />
+                            <InteractionScreen active={!postsOrInt} />
+                            */}
+                        </div>
                     </div>
                 </div>
             </div>
