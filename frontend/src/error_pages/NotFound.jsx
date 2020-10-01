5aa438a54b048e246a23037731eb718a21c39c62
Mark Gindling <mgindlin@purdue.edu>
2020-10-01 16:46:54 -0400

    1.6.1 Mark -- Made Error Page Pretty Wanted to make sure I still know how CSS works.

--- frontend/src/error_pages/NotFound.jsx
+++ frontend/src/error_pages/NotFound.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
-
-//module.exports(isFormValid);
-//module.exports(Login);
+import "./notfound.css";
 
 export default function NotFound() {
   return (
-	  <div>
-		<h2>404: Page Not Found</h2>
-		<p>Looks like you got tangled up, this page doesnt exist!</p>
+	  <div className="error-box">
+		<img className="error-image" src="./img/weave-icon.svg"></img>
+		<h2 className="error-code">404: Page Not Found</h2>
+		<p className="error-message">Looks like you got tangled up, this page doesn't exist!</p>
 	  </div>
   )
 }
