733a6418429950299422395816c605b7e6f680c2
schikyal <42979122+schikyal@users.noreply.github.com>
2020-10-05 15:58:12 -0400

    1.10.1 Sai attached onclick to button rather than img

--- frontend/src/Shared_Components/Post.jsx
+++ frontend/src/Shared_Components/Post.jsx
@@ -85,6 +85,7 @@ export default function Post({
     {/*const { saved, voted } = statedata;*/}
 
     const vote = (value) => {
+        setVoted(value);
         const body = {
             username: userName,
             type: '1',
@@ -107,7 +108,6 @@ export default function Post({
 			if (data.msg) {
 				window.location = "/login"
             }
-            setVoted(value);
         }).catch(err => {
             console.error(err);
 
@@ -150,27 +150,27 @@ export default function Post({
                 <div className="post-vote-container">
                     {/* replace with upvote and downvote */}
                     {voteCheck > 0 ? (
-                        <button>Un<img src="/img/weave-icon.svg" className="post-vote-pic" alt="" onClick={() => vote(0)} /></button>
+                        <button onClick={() => vote(0)} >Un<img src="/img/weave-icon.svg" className="post-vote-pic" alt="" /></button>
                     
                     ) : (
-                        <button>U<img src="/img/weave-icon.svg" className="post-vote-pic" alt="" onClick={() => vote(1)} /></button>
+                        <button onClick={() => vote(1)} >U<img src="/img/weave-icon.svg" className="post-vote-pic" alt="" /></button>
                     )}
                     <p>{upvoteCount}</p>
                     <p>{downvoteCount}</p>
                     {voteCheck < 0 ? (
-                        <button>Dn<img src="/img/weave-icon.svg" className="post-vote-pic" alt="" onClick={() => vote(0)} /></button>
+                        <button onClick={() => vote(0)}>Dn<img src="/img/weave-icon.svg" className="post-vote-pic" alt="" /></button>
                     ) : (
-                        <button>D<img src="/img/weave-icon.svg" className="post-vote-pic" alt="" onClick={() => vote(-1)} /></button>
+                        <button onClick={() => vote(-1)}>D<img src="/img/weave-icon.svg" className="post-vote-pic" alt="" /></button>
                     )}
                 </div>
                 <div className="post-text-container">
+                    <h1>{voteCheck}</h1>
                     {/* align these better later */}
                     <p className="post-text">Creator</p>
                     <p className="post-text">{date_created}</p>
                     <p className="post-text">{topic_name}</p>
                     <h1 className="post-title">{title}</h1>
                     <p className="post-text">{content}</p>
-                    <h1>{saveCheck}</h1>
                     {saveCheck === -1 ? (
                         <button className="post-save-button" onClick={() => savePost(1)}>Save</button>
                     ) : (
