752b2a65b0e13c4f8aff29d2d55f70b184565f63
Sahil Kapur <sahilkapur.a@gmail.com>
2020-10-15 01:14:02 -0400

    refactor profile

---
+++ frontend/src/Shared_Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect } from "react";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  const [xPosition, setX] = React.useState(-250);
+
+  useEffect(() => {
+    setX(0);
+  }, []);
+
+  const toggleMenu = () => {
+    if (xPosition < 0) {
+      setX(0);
+    } else {
+      setX(-250);
+    }
+  };
+
+  //delete JWT token and refresh
+  const logout = () => {
+    const access_token = localStorage.getItem('access_token');
+    const logoutEndpoint = "http://localhost:5000/logout";
+    fetch(logoutEndpoint, {
+      method: "DELETE",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + access_token
+      },
+    }).then(response => response.json()).then(data => {
+      const { refresh_token } = data;
+      const endpoint2 = "http://localhost:5000/logout2";
+      fetch(endpoint2, {
+        method: "DELETE",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + refresh_token
+        },
+      }).then(response => response.json()).then(data => {
+        localStorage.clear("access_token");
+        localStorage.clear("refresh_token");
+        window.location = "../"
+      }).catch(err => {
+        console.error(err);
+        alert("error: check console for details");
+      });
+    }).catch(err => {
+      console.error(err);
+      alert("error: check console for details");
+    });
+
+  };
+
+  return (
+    <>
+      <div
+        className="sidebar"
+        style={{ transform: `translatex(${xPosition}px)` }}
+      >
+        <button
+          onClick={() => toggleMenu()}
+          className="profile-toggle-menu"
+          style={{
+            transform: `translate(${250}px, 20vh)`
+          }}
+        ></button>
+        <div className="side-bar-menu">
+          {/* These need to link to actual pages eventually */}
+          <h1 className="side-bar-heading">Explore your web:</h1>
+          <a href="../timeline/" className="side-bar-selection">Your timeline</a> <br />
+          <a href="" className="side-bar-selection">Saved posts</a> <br />
+          <button className="side-bar-logout" onClick={(e) => logout(e)}>Logout</button>
+        </div>
+      </div>
+    </>
+  );
+}
