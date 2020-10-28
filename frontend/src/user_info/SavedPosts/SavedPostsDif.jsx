8011d33bbc450cb5273877fb79a7d898822b8b87
schikyal <42979122+schikyal@users.noreply.github.com>
2020-10-28 01:22:06 -0400

    Sai Saved Post Paginate (need page count)

---
+++ frontend/src/user_info/SavedPosts/SavedPostsDif.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import NavBar from "../../Shared_Components/NavBar";
+import Post from "../../Shared_Components/Post/Post";
+import ReactPaginate from 'react-paginate';
+
+import './savedposts.css';
+
+export default function SavedPosts() {
+  const [postData, setPostData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pageCount, setPageCount] = useState(0)
+  const [perPage] = useState(5);
+  const access_token = localStorage.getItem('access_token');
+  if (access_token == null) {
+    window.location = "/login"
+  }
+  useEffect(() => {
+    const endpoint = "http://localhost:5000/savedposts/"
+    const offset = currentPage * perPage
+    fetch(endpoint, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + access_token
+      },
+      body: JSON.stringify({
+        start: offset,
+        end: offset + perPage
+      })
+    }).then(response => response.json()).then(data => {
+	    const { pull_list: post_ids } = data;
+      setPostData(post_ids);
+    }).catch(err => {
+      console.log(err);
+      alert("Error in console");
+    });
+  }, [newPage]);
+
+  let postsContent = [];
+  postData.forEach((postId) => {
+    postsContent.push(<Post postId={postId} userName={"sahilkapur"} />)
+  });
+  if (postsContent.length === 0) {
+    postsContent = <p>No Posts!</p>;
+  }
+
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setCurrentPage(selectedPage);
+  };
+  
+  return (
+    <div>
+        <NavBar />
+        {data}
+       <ReactPaginate
+                    previousLabel={"<"}
+                    nextLabel={">"}
+                    pageCount={pageCount}
+                    marginPagesDisplayed={1}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
+    </div>
+  );
+}
