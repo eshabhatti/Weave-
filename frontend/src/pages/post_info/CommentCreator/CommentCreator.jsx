edb513e613de9570969c28366987b97c49a92a2c
Sahil Kapur <sahilkapur.a@gmail.com>
2020-11-02 17:12:45 -0500

    remove error message on change

--- frontend/src/pages/post_info/CommentCreator/CommentCreator.jsx
+++ frontend/src/pages/post_info/CommentCreator/CommentCreator.jsx
@@ -61,6 +61,7 @@ export default function CommentCreator({
             onChange={e => {
               updateCommentContent(e.target.value);
               updateSuccessMessage("");
+              updateErrorMessage("");
             }}
             className="comment-form-input-body"
           />
