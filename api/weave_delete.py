b79e48c59318866df4553e82b1023508dc48ce54
Mark Gindling <mgindlin@purdue.edu>
2020-11-15 16:20:41 -0500

    3.0.2 Mark -- Delete Validation Locked account deletion behind a password check.

--- api/weave_delete.py
+++ api/weave_delete.py
@@ -2,28 +2,45 @@ from datetime import datetime
 from flask import Blueprint, request, jsonify, current_app, send_file
 from extensions import mysql
 from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token, create_refresh_token
-from werkzeug.utils import secure_filename
-from os import path
-import re
+import bcrypt
 
 weave_delete = Blueprint('weave_delete', __name__)
 
 
 # # # # Backend code for account deletion.
-# # Expects a GET request and a JWT token.
+# # Expects a POST request and a JWT token, along with the user's current password.
 # # Returns JSON that tells whether the account was successfully deleted.
-@weave_delete.route("/deleteaccount/", methods=["GET"])
+@weave_delete.route("/deleteaccount/", methods=["POST"])
 @jwt_required
 def weave_delete_account():
     
-    # The backend has received a DELETE request.
-    if request.method == "GET":
+    print("HELLO EVERYONE") # DEBUGGING
+
+    # The backend has received a POST request.
+    if request.method == "POST":
         
         # Initializes MySQL cursor.
         cursor = mysql.connection.cursor()
 
+        # Checks for JSON format.
+        if (not request.is_json):
+            return jsonify({'error_message': 'Request Error: Not JSON.'})
+        user_info = request.get_json()
+
+        # Checks for password.
+        if ("password" not in user_info):
+            return jsonify({'error_message': 'Request Error: Missing JSON Element'}), 400
+
         # Gets username in JSON format.
-        user_info = {"username" : get_jwt_identity()}
+        user_info["username"] = get_jwt_identity()
+
+        # Checks password before allowing the account to be deleted.
+        cursor.execute("SELECT encrypted_password FROM UserAccount WHERE username = %s;", (user_info["username"],))
+        correct_password = cursor.fetchall()[0]["encrypted_password"]
+        attempted_password = user_info["password"]
+        valid_password = bcrypt.checkpw(attempted_password.encode('utf8'), correct_password.encode('utf8')) 
+        if (not valid_password):
+            return jsonify({'error_message':'Password incorrect.'}), 401
 
         # Changes posts made by user to show DELETED as creator
         post_delete_query = 'UPDATE Post SET creator = "DELETED" WHERE creator = %s;'
@@ -52,5 +69,7 @@ def weave_delete_account():
         }
         return jsonify(ret), 200
 
+    else:
+        return jsonify({'error_message': 'Request Error: Not POST Request'}), 400
 
  
--- frontend/src/user_info/Settings/Settings.jsx
+++ frontend/src/user_info/Settings/Settings.jsx
@@ -6,61 +6,85 @@ import Sidebar from '../../Shared_Components/Sidebar/Sidebar';
 import './settings.css';
 
 export default function Settings(){
-	
+
 	const access_token = localStorage.getItem('access_token');
+	const [currPass, updateCurrPass] = useState("");
+	const [newPass, updateNewPass] = useState("");
+	const [newUser, updateNewUser] = useState("");
+	const [newEmail, updateNewEmail] = useState("");
+	const [errorMessage, updateErrorMessage] = useState("");
 	if (access_token == null) {
 		window.location = '/login';
 	}
 
 	// Delete Account Function
-	const DeleteAccount = () => {
-		fetch('http://localhost:5000/deleteaccount/', {
-      		method: 'GET',
+	const DeleteAccount = (event) => {
+		
+		event.preventDefault();
+		const JSONbody = {
+			password: currPass,
+		}
+
+		const deletepoint = "http://localhost:5000/deleteaccount/";
+		fetch(deletepoint, {
+      		method: 'POST',
       		headers: {
         		'Content-Type': 'application/json',
         		'Authorization': 'Bearer ' + access_token
-      		},
-    	}).then(response => response.json()).then(data => {
-      		const errorMessage = data.error_message || data.msg;
-      		if (errorMessage) {
-        		alert(errorMessage);
-	  		}
-			  
-			// Blacklists the current JWT tokens; mirrors logout.
-			const access_token = localStorage.getItem('access_token');
-			const logoutEndpoint = "http://localhost:5000/logout";
-			fetch(logoutEndpoint, {
-			  method: "DELETE",
-			  headers: {
-				'Content-Type': 'application/json',
-				'Authorization': 'Bearer ' + access_token
 			  },
-			}).then(response => response.json()).then(data => {
-			  const { refresh_token } = data;
-			  const endpoint2 = "http://localhost:5000/logout2";
-			  fetch(endpoint2, {
+			  body: JSON.stringify(JSONbody)
+    	}).then(response => response.json()).then(data => {
+			
+			// alert("Request sent!")  
+			const errorMessage = data.error_message || data.msg;
+      		if (errorMessage) {
+				// alert(errorMessage);
+				updateErrorMessage(data.error_message);
+			}
+
+			else {
+				
+				// Blacklists the current JWT tokens; mirrors logout.
+				const access_token = localStorage.getItem('access_token');
+				const logoutEndpoint = "http://localhost:5000/logout";
+				fetch(logoutEndpoint, {
 				method: "DELETE",
 				headers: {
-				  'Content-Type': 'application/json',
-				  'Authorization': 'Bearer ' + refresh_token
+					'Content-Type': 'application/json',
+					'Authorization': 'Bearer ' + access_token
 				},
-			  }).then(response => response.json()).then(data => {
-				localStorage.clear("access_token");
-				localStorage.clear("refresh_token");
-				window.location = "/login";
-			  }).catch(err => {
+				}).then(response => response.json()).then(data => {
+				const { refresh_token } = data;
+				const endpoint2 = "http://localhost:5000/logout2";
+				fetch(endpoint2, {
+					method: "DELETE",
+					headers: {
+					'Content-Type': 'application/json',
+					'Authorization': 'Bearer ' + refresh_token
+					},
+				}).then(response => response.json()).then(data => {
+					localStorage.clear("access_token");
+					localStorage.clear("refresh_token");
+					window.location = "/login";
+				}).catch(err => {
+					console.error(err);
+					alert("error: check console for details");
+				});
+				}).catch(err => {
 				console.error(err);
 				alert("error: check console for details");
-			  });
-			}).catch(err => {
-			  console.error(err);
-			  alert("error: check console for details");
-			});
+				});
+				// End JWT Blacklist
+			
+			}  
+
     	}).catch(err => {
       		console.error(err);
     	});
 	}
 	// End Delete Account
+
+	const errObject = errorMessage !== "" ? <ErrorBubble message={errorMessage} /> : null;
 
 	return (
 	    <div>
@@ -73,18 +97,46 @@ export default function Settings(){
 						<form className="settings-form">
 
 							<label className="settings-label">Enter your current password:</label>
-							<input className="settings-input" />
+							<input 
+								value={currPass}
+								onChange={e => {
+									updateCurrPass(e.target.value);
+									updateErrorMessage("");
+								}}
+								className="settings-input" 
+							/>
 
 							<hr className="settings-divider"/>
 
 							<label className="settings-label">Enter a new password:</label>
-							<input className="settings-input" />
+							<input 
+								value={newPass}
+								onChange={e => {
+									updateNewPass(e.target.value);
+									updateErrorMessage("");
+								}}
+								className="settings-input" 
+							/>
 
 							<label className="settings-label">Enter a new username:</label>
-							<input className="settings-input" />
+							<input 
+								value={newUser}
+								onChange={e => {
+									updateNewUser(e.target.value);
+									updateErrorMessage("");
+								}}
+								className="settings-input" 
+							/>
 
 							<label className="settings-label">Enter a new email:</label>
-							<input className="settings-input" />
+							<input 
+								value={newEmail}
+								onChange={e => {
+									updateNewEmail(e.target.value);
+									updateErrorMessage("");
+								}}
+								className="settings-input" 
+							/>
 
 							<button className="settings-button">Update Settings</button>
 
@@ -94,10 +146,18 @@ export default function Settings(){
 
 							<button className="delete-button" onClick={(e) => DeleteAccount(e)}>Delete My Account</button>
 						</form>
+						{errObject}
 					</div>
 				</div>
 			</div>
 		</div>
 	);
-
 } 
+
+function ErrorBubble({ message }) {
+	return (
+	  <div className="edit-error-bubble">
+		<p className="edit-error-message">{message}</p>
+	  </div>
+	)
+  }
