a04adf728faf9c71f6956d1e68a7bb90c6d08494
Sahil Kapur <sahilkapur.a@gmail.com>
2020-11-24 23:01:54 -0800

    add build to repo

---
+++ frontend/build/precache-manifest.07ac0a02fea13607d3b2fcdec8e5b626.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebb6a3b95a58a6dbf650ef4e9486440",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7cb66c1c244b2a8c7b1",
+    "url": "/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "59ab983f18ce92f00ab6",
+    "url": "/static/css/main.a2b70ae3.chunk.css"
+  },
+  {
+    "revision": "b7cb66c1c244b2a8c7b1",
+    "url": "/static/js/2.3c077a08.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3c077a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ab983f18ce92f00ab6",
+    "url": "/static/js/main.3e06bbfc.chunk.js"
+  },
+  {
+    "revision": "949794f6654d7f6ae16e",
+    "url": "/static/js/runtime-main.aa57ce67.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
