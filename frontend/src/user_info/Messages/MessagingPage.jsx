90e1e023d6958815dd3d921e614a357da1dc70c4
schikyal <42979122+schikyal@users.noreply.github.com>
2020-11-25 21:55:21 -0500

    Sai - MessagingPage has input now

--- frontend/src/user_info/Messages/MessagingPage.jsx
+++ frontend/src/user_info/Messages/MessagingPage.jsx
@@ -5,15 +5,97 @@ import Sidebar from '../../Shared_Components/Sidebar/Sidebar';
 import Feed from "../../Shared_Components/Feed/Feed";
 
 export default function MessagingPage(){	
-	
+	const [message, uploadChatMessage] = useState("")
+  const [receiver, setReceiver] = useState("")
+  const [errorMessage, updateErrorMessage] = useState("");
+  const access_token = localStorage.getItem('access_token');
+  if (access_token == null) {
+    window.location = "/login"
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (isMessageValid({ message: message, receiver: receiver, updateErrorMessage })) {
+      const body = {
+        content: message,
+        receiver: receiver,
+      }
+      console.log(body)
+      const endpoint = (process.env.NODE_ENV === 'production' ? "http://weave.projectcarbon.io/server" : "http://localhost:5000") + "/createmessage/";
+      fetch(endpoint, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + access_token
+        },
+        body: JSON.stringify(body)
+      }).then(response => response.json()).then(data => {
+        if (data.error_message) {
+          updateErrorMessage(data.error_message);
+        } else {
+
+        }
+      }).catch(err => {
+        console.error(err);
+        alert("error: check console for details");
+      });
+    }
+  }
+
+  const errObject = errorMessage !== "" ? <ErrorBubble message={errorMessage} /> : null;
+  
 	return (
-	    <div>
-		<NavBar />
-			<div className="settings-container">
-				<Sidebar />	
-				<Feed route="allmessages/" elementType="messages"/>
+		<div>
+		  <NavBar />
+		  <div className="chat-container">
+			<Sidebar />
+			<div className="chat-content">
+			  <Feed route="allmessages/" elementType="messages"/>
+			  <form className="chat-form">
+				<label className="chat-form-label">Message</label>
+				<input
+				  value={message}
+				  onChange={e => {
+					uploadChatMessage(e.target.value);
+					updateErrorMessage("");
+				  }} className="chat-form-input"
+				/>
+				<label className="chat-form-label">Receiver</label>
+				<input
+				  value={receiver}
+				  onChange={e => {
+					setReceiver(e.target.value);
+					updateErrorMessage("");
+				  }} className="chat-form-input"
+				/>
+				<button type="submit" className="chat-submit-btn" onClick={(e) => onSubmit(e)}>Send Message</button>
+			  </form>
+			  {errObject}
 			</div>
+		  </div>
 		</div>
-	);
+	  )
+	}
+	
+	function isMessageValid({ message, receiver, updateErrorMessage }) {
+	  if (message === "") {
+		updateErrorMessage("Please enter a message.");
+	  }
+	  else if (receiver === "") {
+		updateErrorMessage("Please enter a receiver.");
+	  }
+	  else {
+		return true;
+	  }
+	  return false;
+	}
+	
+	function ErrorBubble({ message }) {
+	  return (
+		<div className="chat-error-bubble">
+		  <p className="chat-error-message">{message}</p>
+		</div>
+	  )
+	}
 
 }
