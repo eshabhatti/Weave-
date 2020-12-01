dab4cdc273253764f9e4fe6884e53f355108c03f
Sahil Kapur <sahilkapur.a@gmail.com>
2020-12-01 04:33:47 -0800

    recreate build

---
+++ frontend/build/precache-manifest.8d3d4ed5aab623a3fb8f042947d9ff7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa6881e85694b56af5715431fd420478",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd796bf4b643e19494bb",
+    "url": "/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "ed8631b6c68f1f4dbd99",
+    "url": "/static/css/main.1bb1590f.chunk.css"
+  },
+  {
+    "revision": "bd796bf4b643e19494bb",
+    "url": "/static/js/2.69377840.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.69377840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8631b6c68f1f4dbd99",
+    "url": "/static/js/main.0ab23479.chunk.js"
+  },
+  {
+    "revision": "949794f6654d7f6ae16e",
+    "url": "/static/js/runtime-main.aa57ce67.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
